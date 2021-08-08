4acf1d0f4e52b1cdb3af09f658af747f4fdcefea
Neil Van <nvq.alino@gmail.com>
2021-08-08 22:10:11 +0800

    add 'users' command

--- lihim/main.py
+++ lihim/main.py
@@ -1,20 +1,23 @@
+from typing import List
 import typer
 
 app = typer.Typer()
 
+user_list = ['neeban','neil']
 group_list = ['aws','azure','gcp']
 
 @app.command()
-def groups():
-    typer.echo(print_groups())
+def users():
+    typer.echo(print_list(user_list))
 
 @app.command()
-def notes():
-    typer.echo(group_list)
+def groups():
+    typer.echo(print_list(group_list))
 
-def print_groups():
-    for group in group_list:
-        print(group)
+
+def print_list(list_type: List[str]):
+    for item in list_type:
+        print(item)
 
 def main():
     pass

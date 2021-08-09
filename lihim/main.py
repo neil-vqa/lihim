6cf79fda478bba51777325465f71578183714eff
Neil Van <nvq.alino@gmail.com>
2021-08-09 17:11:52 +0800

    add user login and management commands

--- lihim/main.py
+++ lihim/main.py
@@ -1,31 +1,60 @@
 from typing import List
 import typer
-from .db import create
+from .db import create_db, checker, create_user, check_users, enter_user
+
 
 app = typer.Typer()
 
-user_list = ['neeban','neil']
-group_list = ['aws','azure','gcp']
-
-@app.command()
-def users():
-    typer.echo(print_list(user_list))
-
-@app.command()
-def groups():
-    typer.echo(print_list(group_list))
 
 @app.command()
 def initdb():
-    create()
-    print("Database created.")
+    create_db()
+    typer.echo("Database created.")
 
-def print_list(list_type: List[str]):
-    for item in list_type:
-        print(item)
+@app.command()
+def check():
+    # check logged in user
+    typer.echo(f"Current user:")
 
-def main():
-    pass
+@app.command()
+def useradd():
+    username = typer.prompt("Username")
+    passwordx = typer.prompt("Password", hide_input=True)
+    passwordy = typer.prompt("Retype password", hide_input=True)
+
+    if passwordx == passwordy:
+        try:
+            create_user(username, passwordx)
+            typer.echo(f"User {username} created.")
+        except Exception as e:
+            typer.echo(f"User {username} already exists.")
+    else:
+        typer.echo("Password did not match. Please try again.")
+
+@app.command()
+def users():
+    users_list = check_users()
+    for user in users_list:
+        typer.echo(user.username)
+
+@app.command()
+def login(
+    username: str, 
+    password: str = typer.Option(
+        ..., prompt=True, hide_input=True
+    )
+):
+    try:
+        enter_user(username, password)
+        typer.echo(f"Logged in.")
+    except Exception as e:
+        typer.echo(e)
+    
+
+# @app.command()
+# def groups():
+#     typer.echo(print_list(group_list))
+
 
 if __name__ == "__main__":
     app()
